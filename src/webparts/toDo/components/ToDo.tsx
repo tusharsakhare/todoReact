2c9cfb927ee87a99ee357d05a3e2c1d94492efa1
Tushar Sakhare <tushar.sakhare@imaginea.com>
2020-01-14 19:53:21 +0530

    Event Handler when a card moved from one lane to another

--- src/webparts/toDo/components/ToDo.tsx
+++ src/webparts/toDo/components/ToDo.tsx
@@ -20,7 +20,7 @@ export default class ToDo extends React.Component<IToDoProps, IToDoState, {}> {
       contributoritems: [],
       todoitems: [],
       boarddata: {
-        lanes:[],
+        lanes: [],
       }
     };
   }
@@ -30,9 +30,16 @@ export default class ToDo extends React.Component<IToDoProps, IToDoState, {}> {
       <div className={styles.toDo}>
         <h2 className={styles.header}>{this.props.header}</h2>
         <h3>{this.state.status}</h3>
-        <Board data={this.state.boarddata} className= {styles.boardContainer} />
+        <Board data={this.state.boarddata}
+          className={styles.boardContainer}
+          onCardMoveAcrossLanes={this.onCardMoveAcrossLanes} />
       </div>
     );
+  }
+
+  private onCardMoveAcrossLanes(fromProjectId, toProjectId, toDoId, addedIndex) {
+    debugger;
+    console.log('onCardMoveAcrossLanes: ${fromProjectId}, ${toProjectId}, ${toDoId}, ${addedIndex}');
   }
 
   private convertTodoIntoBoardData() {
@@ -40,15 +47,23 @@ export default class ToDo extends React.Component<IToDoProps, IToDoState, {}> {
       lanes: [],
     };
 
-    const tempLanes = this.state.todoitems.map(q => q.Project.Title);
-    var tempLanesUnique = tempLanes.filter((q, idx) => tempLanes.indexOf(q) === idx);
+    const tempLanes = this.state.todoitems.map(q => q.Project);
+    var tempLanesUnique = [];
+    var tempLanesUniqueObjArr=[];
 
+    tempLanes.map(lane => {
+      if (tempLanesUnique.indexOf(lane.Title) === -1) {
+        tempLanesUnique.push(lane.Title)
+        tempLanesUniqueObjArr.push({Title:lane.Title, Id:lane.Id});
+      }
+    });
+    debugger;
     for (var i = 0; i < tempLanesUnique.length; i++) {
       var tempContributors = this.state.todoitems.filter(item => item.Project.Title === tempLanesUnique[i]);
       var tempcards = [];
       for (var j = 0; j < tempContributors.length; j++) {
         var tempCard = {
-          id: 'Card' + i + j,
+          id: tempContributors[j].Id,
           title: tempContributors[j].Contributor.Title,
           description: tempContributors[j].Title,
           label: tempContributors[j].Status,
@@ -57,11 +72,10 @@ export default class ToDo extends React.Component<IToDoProps, IToDoState, {}> {
         tempcards.push(tempCard)
       }
 
-      
 
       var tempBoard = {
-        id: 'lane' + i,
-        title: tempLanesUnique[i],
+        id: tempLanesUniqueObjArr[i].Id,
+        title: tempLanesUniqueObjArr[i].Title,
         label: tempcards.length + '/' + tempcards.length,
         cards: tempcards
       };
@@ -85,7 +99,7 @@ export default class ToDo extends React.Component<IToDoProps, IToDoState, {}> {
       status: 'Loading latest items...'
     });
 
-    this.props.spHttpClient.get(`${this.props.siteUrl}/_api/web/lists/getbytitle('ToDo')/items?$select=Title,Id,Status,Project/Title,Contributor/Title&$expand=Project,Contributor`,
+    this.props.spHttpClient.get(`${this.props.siteUrl}/_api/web/lists/getbytitle('ToDo')/items?$select=Title,Id,Status,Project/Title,Project/Id,Contributor/Title,Contributor/Id&$expand=Project,Contributor`,
       SPHttpClient.configurations.v1,
       {
         headers: {
